3c0c8763a05f72c5d152eaa47278506bdac2c865
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-03-21 17:19:16 -0400

    add write to file

--- minimum_dependency_generator/main.py
+++ minimum_dependency_generator/main.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 
 from minimum_dependency_generator import generate_min_requirements
 
@@ -15,11 +15,19 @@ def main():
     parser.add_argument('--extras_require', nargs='+', default=None,
                         help='path for requirements to minimize')
 
+    parser.add_argument('--output_filepath', default=None,
+                        help='path to output minimum dependencies (optional)')
+
     args = parser.parse_args()
     requirements = generate_min_requirements(args.paths, args.options, args.extras_require)
     requirements = sanitize_string(requirements)
     # DO NOT remove, the GH action needs to output
     print("::set-output name=min_reqs::{}".format(requirements))
+
+    if args.output_filepath and os.path.endswith(".txt"):
+        with open(filename, 'w') as f: # OSError if file exists or is invalid
+            f.write(requirements)
+
 
 
 def sanitize_string(s):
